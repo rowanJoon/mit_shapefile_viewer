1c5fbe79450e4931a660ac96c2f33e207f0b52ab
맵인프라 김현준 <rowan.joon@tmapmobility.com>
2023-12-11 14:57:40 +0900

    `.shp` 파일 검증 로직 작성

--- src/index.ts
+++ src/index.ts
@@ -7,48 +7,61 @@ interface BoundingBox {
     yMax: number;
 }
 
-function readIntLE(buffer: Buffer, offset: number): number {
-    return buffer.readUInt32LE(offset);
+interface ShapeFileHeader {
+    fileCode: number;
+    fileLength: number;
+    version: number;
+    shapeType: number;
+    boundingBox: BoundingBox;
 }
 
-function readDoubleLE(buffer: Buffer, offset: number): number {
-    return buffer.readDoubleLE(offset);
-}
-
-// TODO: bounding box 변수 offset number 정보 확인 필요
 function readBoundingBox(buffer: Buffer): BoundingBox {
     const boundingBox: BoundingBox = {
-        xMin: readDoubleLE(buffer, 4),
-        yMin: readDoubleLE(buffer, 12),
-        xMax: readDoubleLE(buffer, 20),
-        yMax: readDoubleLE(buffer, 28)
+        xMin: buffer.readDoubleLE(0),
+        yMin: buffer.readDoubleLE(8),
+        xMax: buffer.readDoubleLE(16),
+        yMax: buffer.readDoubleLE(24)
     };
 
     return boundingBox;
 }
 
-function readShapefile(filePath: string): void {
+function readShapefileHeader(filePath: string): ShapeFileHeader {
     const buffer: Buffer = fs.readFileSync(filePath);
-    const fileCode: number = readIntLE(buffer, 0);
+    const boundingBoxBuffer: Buffer = Buffer.alloc(32);
+    buffer.copy(boundingBoxBuffer, 0, 36, 68);
 
-    // shape file 시작을 나타내는 4byte 정수, 9994 로 시작
-    if (fileCode !== 9994) {
+    const header: ShapeFileHeader = {
+        fileCode: buffer.readInt32BE(0),
+        fileLength: buffer.readInt32LE(24),
+        version: buffer.readInt32LE(28),
+        shapeType: buffer.readInt32LE(32),
+        boundingBox: readBoundingBox(boundingBoxBuffer)
+    };
+
+    return header;
+}
+
+function validateShapefile(filePath: string): void {
+    const header: ShapeFileHeader = readShapefileHeader(filePath);
+
+    if (
+        header.fileCode !== 9994 ||
+        header.version !== 1000 ||
+        header.shapeType < 0 ||
+        header.shapeType > 31
+    ) {
         console.error('Invalid Shapefile format');
         return;
     }
 
-    const fileLength: number = readIntLE(buffer, 24); // 파일 길이, 16비트 워드 단위로 나타내는 4byte 정수
-    const version: number = buffer.readInt32LE(28); // shape file 의 버전을 나타내는 4byte 정수
-    const shapeType: number = buffer.readInt32LE(32); // 공간 데이터 유형, shape file 에 저장된 공간 데이터의 유형을 나타내는 4byte 정수
-    const boundingBox: BoundingBox = readBoundingBox(buffer.slice(36, 68)); // 경계 상자, 파일에 포함된 모든 공간 데이터의 범위를 나타내는 정보
-
-    console.log('File Code: ', fileCode);
-    console.log('File Length: ', fileLength);
-    console.log('Version: ', version);
-    console.log('Shape Type: ', shapeType);
-    console.log('Bounding Box: ', boundingBox);
+    console.log('File Code: ', header.fileCode);
+    console.log('File Length: ', header.fileLength);
+    console.log('Version: ', header.version);
+    console.log('Shape Type: ', header.shapeType);
+    console.log('Bounding Box: ', header.boundingBox);
 }
 
 // TODO: html 내 file Reader 로 파일 읽어들여 path 값 추출하여 삽입
-const shapefilePath: string = '';
-readShapefile(shapefilePath);
+const shapefilePath: string = '/Users/t1100423/Downloads/turkey/Hatlar.shp';
+validateShapefile(shapefilePath);
